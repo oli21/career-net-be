5b92fc3715e3975ccf7169a1161a41a793237b5e
Md Rubaith Adnan <rubaith@Mds-MacBook-Air.local>
2023-02-10 19:14:20 +0600

    added helmet, cors,xss and rate limiter

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+//Extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -14,7 +21,18 @@ const jobsRouter = require("./routers/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windoMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 // routes
@@ -34,7 +52,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    console.log("Connecting to database.....")
+    console.log("Connecting to database.....");
     await connect(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
