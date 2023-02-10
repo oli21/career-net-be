116c39ce5379912178f510365fa40b198c900860
Md Rubaith Adnan <rubaith@Mds-MacBook-Air.local>
2023-02-10 22:57:25 +0600

    Added swagger documentation for api

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+//swagger
+
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -21,6 +27,7 @@ const jobsRouter = require("./routers/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// extra packages
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
@@ -33,12 +40,12 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// extra packages
-
 // routes
 app.get("/", (req, res) => {
-  res.send("jobs api");
+  res.send("<h1>Jobs Api</h1><a href='/api-docs'>Documentation</a>");
 });
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobsRouter);
 
